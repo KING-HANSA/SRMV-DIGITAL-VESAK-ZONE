5b6de6b2246587bdfe20de8ba8788f0bd23f9947
KING-HANSA <hansadewmina2008@gmail.com>
2025-04-17 18:07:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-// Example JS: Just logging to console
-console.log("Page loaded successfully.");
+function toggleMenu() {
+  const nav = document.getElementById('nav-links');
+  nav.classList.toggle('active');
+}
