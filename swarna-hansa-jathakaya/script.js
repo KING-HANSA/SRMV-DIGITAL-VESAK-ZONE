04c943e53e259d7340e312c71e7093df9c7f4fbd
KING-HANSA <hansadewmina2008@gmail.com>
2025-04-17 19:00:14 +0530

    Update script.js

--- swarna-hansa-jathakaya/script.js
+++ swarna-hansa-jathakaya/script.js
@@ -4,8 +4,6 @@
 
       function updateCountdown() {
         loadingDiv.innerHTML = "<h1 style='color:yellow'>ස්වර්ණහංස ජාතක කතා වස්තුව</h1>" +
-                               "<h3>වෙසක් තොරණ</h3>" +
-                               "<h4>හරිත දුෂ්මන්ත සේනාධීර ගේ නිර්මාණයකි</h4>" +
                                 "<p>තොරණේ ඇති එක් එක් කොටුව මතට Mouse එක රැගෙන යාමෙන් එම කොටුවේ විස්තරය බලාගැනීමට හැකිය. කොටුව මත Click කිරීමෙන් අදාළ කොටුව පමණක් දර්ශණය කරගත හැකිය.</p>"+
                                "<p>Thorana will show in " + countdown + " seconds...</p>";
 
